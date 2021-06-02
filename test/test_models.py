149e88d636f97e59753c869f93ad772e97fa53e4
Jefferson Kael Marloch <jefferson.marloch@olist.com>
2021-06-02 11:02:01 -0300

    adicionado poetry

--- test/test_models.py
+++ test/test_models.py
@@ -1,2 +1,9 @@
-from  models.category_model import Category
-from  models.product_model import Product
+import unittest
+
+from  ..models.category_model import Category
+from  ..models.product_model import Product
+
+
+class CategoryTest(unittest.TestCase):
+    def test_category_model(self):
+        self.category = Category("Acessórios", "blablabla")
