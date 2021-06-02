67ff89aaadfb9dbc56b798b3d71ceed20709decf
Jefferson Kael Marloch <jefferson.marloch@olist.com>
2021-06-02 17:34:33 -0300

    test

--- test/test_daos.py
+++ test/test_daos.py
@@ -7,6 +7,19 @@ SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 from daos.category_dao import CategoryDAO
+from daos.product_dao import ProdutoDAO
+from models.category_model import Category
+from models.product_model import Product
+
 
 class TestDaos(unittest.TestCase):
+    def test_create_category():
+        categorydao = CategoryDAO("category.txt")
+        category = Category("Acessório", "blablala")
+        categorydao.insert(category.__str__())
 
+
+    def test_create_product():
+        productdao = ProdutoDAO("product.txt")
+        product = Product("Pneu", 900, "blablala", "Acessório")
+        productdao.insert(productdao.__str__())
